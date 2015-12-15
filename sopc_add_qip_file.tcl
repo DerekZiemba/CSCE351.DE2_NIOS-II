ced74c981bc3655329b47fa920fad913ffe997e8
Derek Ziemba <DerekZiemba@gmail.com>
2015-12-14 20:48:58 -0600

    Polished.  Still some issues though like:
    1.) Terminated Thread B shows twice at the end.
    2.) The interrupt handler in prototypeOS.c doesn't get called.

---
+++ sopc_add_qip_file.tcl
@@ -0,0 +1,3 @@
+project_open -current_revision "C:/Users/Derek/Dropbox/School/CSCE351/HW2/ziemba_project1/DE2_NIOS-II-CUSTOM/DE2_Basic_Computer.qpf"
+set_global_assignment -name QIP_FILE "nios_system.qip"
+project_close
